240054af9a96e0b682e7c807459cbf825d7f95de
Julio Cesar Cester <145153358+Juskk59@users.noreply.github.com>
2025-03-17 18:54:06 -0300

    Delete ProjetoControleEstoque directory

--- ProjetoControleEstoque/model/database.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-# Conectar ao MongoDB
-client = MongoClient("mongodb://localhost:27017/")
-
-# Criar banco de dados e coleção
-db = client["estoque_db"]
-collection = db["produtos"]
